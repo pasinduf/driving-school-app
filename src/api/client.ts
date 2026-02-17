bd311a0b1147a523df28e786adb89424128ef73d
pasinduf <pasindu.fernan93@gmail.com>
2026-02-17 19:43:17 +0530

    api fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8020',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
