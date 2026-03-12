d656a7f624b1c2e286035c468533ea04714fcd54
pasinduf <pasindu.fernan93@gmail.com>
2026-03-12 11:12:30 +0530

    style fixes

--- src/components/admin/settings/OthersTab.tsx
+++ src/components/admin/settings/OthersTab.tsx
@@ -67,14 +67,14 @@ export default function OthersTab() {
                         </div>
 
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">Booking Session Duration (Minutes)</label>
+                            <label className="block text-sm font-medium text-gray-700 mb-1">Booking Session Duration (Seconds)</label>
                             <input
                                 type="number"
                                 {...register('bookingSessionDuration', { valueAsNumber: true })}
                                 className="w-full px-4 py-2 border border-gray-200 rounded-lg focus:ring-2 focus:ring-primary/20 focus:border-primary transition-all text-sm"
                             />
                             <p className="text-xs text-gray-400 mt-2 font-light italic">
-                                The locking duration for a booking session. Default is 180 minutes.
+                                The locking duration for a booking session. Default is 180 seconds(3 Minutes).
                             </p>
                         </div>
                     </div>
