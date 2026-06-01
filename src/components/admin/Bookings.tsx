db062c18b149966a4b6ecad2216b23790e298798
pasinduf <pasindu.fernan93@gmail.com>
2026-06-01 11:58:25 +0530

    check commit in vs

--- src/components/admin/Bookings.tsx
+++ src/components/admin/Bookings.tsx
@@ -14,7 +14,7 @@ import BookingDetailsModal from '../BookingDetailsModal';
 export default function Bookings() {
     const { user } = useAuth();
 
-    // Filters & Pagination
+    // Filters & Pagination State
     const [filterDate, setFilterDate] = useState('');
     const [filterInstructorId, setFilterInstructorId] = useState('');
     const [searchInstructorText, setSearchInstructorText] = useState('');
