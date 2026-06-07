f8a1cf1016b72dcf69135d5d81f1309ef4a5b68d
pasinduf <pasindu.fernan93@gmail.com>
2026-06-08 01:08:35 +0530

    single deploy test

--- apps/booking-admin/src/components/layout/TopBar.tsx
+++ apps/booking-admin/src/components/layout/TopBar.tsx
@@ -6,7 +6,7 @@ interface TopBarProps {
     linkLabel?: string;
 }
 
-export default function TopBar({ linkTo = '/', linkLabel = 'Dashboard' }: TopBarProps) {
+export default function TopBar({ linkTo = '/', linkLabel = 'Dashboard11' }: TopBarProps) {
 
     const { company } = useCompany() ?? null;
 
